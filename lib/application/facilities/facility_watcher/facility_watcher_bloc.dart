44d8bb5a919a5579ffc870e10a76f29a54340790
joenaine <joenaine10@gmail.com>
2024-04-05 06:38:37 +0500

    [AppLication] Facility Watcher incompleted

--- lib/application/facilities/facility_watcher/facility_watcher_bloc.dart
+++ lib/application/facilities/facility_watcher/facility_watcher_bloc.dart
@@ -1,9 +1,10 @@
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:localyft/domain/facility/facility_category.dart';
 
 part 'facility_watcher_event.dart';
 part 'facility_watcher_state.dart';
-part '../bloc/facility_watcher_bloc.freezed.dart';
+part 'facility_watcher_bloc.freezed.dart';
 
 class FacilityWatcherBloc
     extends Bloc<FacilityWatcherEvent, FacilityWatcherState> {
--- lib/application/facilities/facility_watcher/facility_watcher_event.dart
+++ lib/application/facilities/facility_watcher/facility_watcher_event.dart
@@ -2,5 +2,12 @@ part of 'facility_watcher_bloc.dart';
 
 @freezed
 class FacilityWatcherEvent with _$FacilityWatcherEvent {
-  const factory FacilityWatcherEvent.started() = _Started;
+  const factory FacilityWatcherEvent.watchByCategory({
+    FacilityCategory? category,
+    @Default(false) bool silent,
+  }) = _WatchByCategory;
+
+  const factory FacilityWatcherEvent.filterFacilites({
+    required List<void> fetchedFacilities,
+  }) = _FilterFacilites;
 }
